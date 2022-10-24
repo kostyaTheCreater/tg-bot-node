6e13ee18939fcb713eb8baee24fcae75e7bc7c3a
kostyaTheCreater <parfilokostya0@gmail.com>
2022-10-24 11:59:37 +0300

    added saving users and little code changes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const telegramApi = require('node-telegram-bot-api');
+const {token, url, port} = require('./config');
+const Pool = require("pg").Pool;
 
-const {token, url} = require('./config');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 const bot = new telegramApi(token,  {
     webHook: {
-        port: 3000
+        port: port
     }
 });
 
@@ -15,16 +22,36 @@ bot.setMyCommands([
     {command: '/info', description: 'Get info'},
 ])
 
-bot.addListener('message', async (msg) => {
+bot.on('message', async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
+    const firstName = msg.from.first_name;
+    const userId = msg.from.id;
+
     if(text == '/start'){
-        await bot.sendMessage(chatId, 'Hello, bro');
-        return;
+        dbConnect(firstName, userId);
+        return bot.sendMessage(chatId, `Hello, ${firstName}`);
     }
     if(text == '/info'){
-        await bot.sendMessage(chatId, `Hello ${msg.from.first_name}`);
-        return;
+        return bot.sendMessage(chatId, `Ur name is ${firstName}?`);
     }
     return bot.sendMessage(chatId, "I dont know, how answer on this");
 })
+
+function dbConnect(firstName, userId){
+    try{
+        pool.connect();
+        pool.query(`SELECT user_id
+        FROM public.users 
+        WHERE user_id = ${userId}`, (err, res) => {
+            if (err) throw err;
+            console.log(JSON.stringify(res.rows));
+            const isExsist = res.rows.length;
+            if(!isExsist){
+                pool.query(`INSERT INTO public.users (id, firstname, user_id) VALUES (DEFAULT, '${firstName}', ${userId})`);
+            }   
+        })
+    }catch(e){
+        console.log(e);
+    }
+}
